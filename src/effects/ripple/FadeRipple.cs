977dbb293722a72c635a1bd7de8564d4cb854b8c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2022-03-14 08:13:09 +0800

    feat(FadeRipple): Initial implementation

--- src/effects/ripple/FadeRipple.cs
+++ src/effects/ripple/FadeRipple.cs
@@ -1,47 +1,64 @@
 class FadeRipple : Ripple {
-	//TODO
-	private const double FADE_DISTANCE = LightKeyManager.KEY_SIZE * 1.5;
+	private const double SLOWEST_SECONDS = 1;
+	private const double UP_MULTIPLIER = 2;
+	private const double EXPONENT = 2.5;
 
-	private const double SECONDS_TO_TRAVEL_WIDTH = 1;
+	private const double FADE_THRESHOLD = 0.5;
+	private const double FADE_DISTANCE_START = LightKeyManager.KEY_SIZE;
+	private const double FADE_DISTANCE_END = LightKeyManager.KEY_SIZE * 2;
 
-	public FadeRipple(LightKey lightKey) : base(lightKey) {}
+	private const double SPEED_START = (LightKeyManager.KEY_SIZE * 30) / AnimationManager.TARGET_FPS;
 
-	private double alphaIntervalFor(Circle lightCircle) {
-		double ringToLightCentre = ring.circumferenceDistanceTo(lightCircle.centre);
-		double lengthOutsideLightCircle = ringToLightCentre - lightCircle.radius;
+	private double linearFactor = 1;
 
-		if (lengthOutsideLightCircle <= 0) return 1;
+	public FadeRipple(LightKey lightKey) : base(lightKey, FADE_DISTANCE_START) {}
 
-		if (lengthOutsideLightCircle > FADE_DISTANCE) return 0;
+	private void addLinearFactor() {
+		double thisFrameReduction = (1 / SLOWEST_SECONDS) / AnimationManager.TARGET_FPS;
+		if (!isStillDown) thisFrameReduction *= UP_MULTIPLIER;
 
-		double proximityToDimmestArc = FADE_DISTANCE - lengthOutsideLightCircle;
-		return proximityToDimmestArc / FADE_DISTANCE;
+		linearFactor -= thisFrameReduction;
 	}
 
-	public override Colour onGetColour(LightKey lightKey) {
-		Colour newColour = colour.clone();
-		newColour.setAlphaInterval(
-			alphaIntervalFor(lightKey.circle)
-		);
-		return newColour;
+	private double getProgressFactor() {
+		double exponentialFactor = Math.Pow(linearFactor, EXPONENT);
+		return Math.Clamp(exponentialFactor, 0, 1);
 	}
 
-	public override void onFrameEnd() {
+	private double getFadeFactor(double progressFactor) {
+		return progressFactor > FADE_THRESHOLD
+			? 1
+			: Math.Clamp(
+				progressFactor / FADE_THRESHOLD,
+				0,
+				1
+			);
+	}
+
+	private double lerp(double start, double end, double factor) {
+		double difference = end - start;
+		return start + (difference * factor);
+	}
+
+	public override bool onFrameEnd() {
+		addLinearFactor();
+		if (linearFactor < 0) return true;
+
+		Console.WriteLine("");
+		Console.WriteLine("LF "+linearFactor);
+		double progressFactor = getProgressFactor();
+		Console.WriteLine("SF "+progressFactor);
+
 		// Expand radius
+		double currentSpeed = SPEED_START * progressFactor;
+		ring.radius += currentSpeed;
 
-		double oneSecondDistance = LightKeyManager.WIDTH / SECONDS_TO_TRAVEL_WIDTH;
-		ring.radius += oneSecondDistance / AnimationManager.TARGET_FPS;
-	}
+		// Update fade & alpha
+		double fadeFactor = getFadeFactor(progressFactor);
+		Console.WriteLine("FF "+fadeFactor);
+		fadeDistance = lerp(FADE_DISTANCE_END, FADE_DISTANCE_START, fadeFactor);
+		alphaMultiplier = fadeFactor;
 
-	public override bool onCheckRemove() {
-		// Remove if exceeds keyboard
-
-		Circle boundingCircle = ring.clone();
-		boundingCircle.radius -= FADE_DISTANCE;
-
-		return boundingCircle.contains(LightKeyManager.TOP_LEFT)
-			&& boundingCircle.contains(LightKeyManager.TOP_RIGHT)
-			&& boundingCircle.contains(LightKeyManager.BOTTOM_LEFT)
-			&& boundingCircle.contains(LightKeyManager.BOTTOM_RIGHT);
+		return false;
 	}
 }
