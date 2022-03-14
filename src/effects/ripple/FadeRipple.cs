ae2937241edf391ff3e82ad824a59760138b38af
Cloud <44526554+Cloud7050@users.noreply.github.com>
2022-03-14 09:44:47 +0800

    feat(FadeRipple): Scale against last frame happening as invisible, more accurate calculations against int frames, tweak fade end

--- src/effects/ripple/FadeRipple.cs
+++ src/effects/ripple/FadeRipple.cs
@@ -1,33 +1,33 @@
 class FadeRipple : Ripple {
 	private const double RADIUS_START = LightKeyManager.KEY_SIZE;
 	private const double RADIUS_END = LightKeyManager.KEY_SIZE * 3;
-	private const double RADIUS_MAX_FRAMES = AnimationManager.TARGET_FPS * 0.6;
-	private const double RADIUS_EXPONENT = 5;
+	private const int RADIUS_MAX_FRAMES = (int) (AnimationManager.TARGET_FPS * 0.6);
+	private const int RADIUS_EXPONENT = 5;
 
 	private const double FADE_START = 1;
-	private const double FADE_END = 2;
-	private const double FADE_FRAMES = AnimationManager.TARGET_FPS * 0.4;
-	private const double FADE_EXPONENT = 4;
+	private const double FADE_END = 2.5;
+	private const int FADE_FRAMES = (int) (AnimationManager.TARGET_FPS * 0.4);
+	private const int FADE_EXPONENT = 4;
 
-	private double radiusFrames = 0;
-	private double fadeFrames = 0;
+	private int upcomingRadiusFrame = 0;
+	private int upcomingFadeFrame = 0;
 
 	public FadeRipple(LightKey lightKey) : base(lightKey, FADE_START) {}
 
 	private void mayExpandRadius() {
-		if (radiusFrames >= RADIUS_MAX_FRAMES) return;
+		if (upcomingRadiusFrame >= RADIUS_MAX_FRAMES) return;
 
-		double frameProgress = radiusFrames / RADIUS_MAX_FRAMES;
+		double frameProgress = upcomingRadiusFrame / (double) (RADIUS_MAX_FRAMES - 1);
 		// Decreasing deceleration
 		double exponential = Math.Pow(1 - frameProgress, RADIUS_EXPONENT);
 		// Starting at 1 (second value) to 0 (first value)
 		ring.radius = MathUtilities.lerp(RADIUS_END, RADIUS_START, exponential);
 
-		radiusFrames++;
+		upcomingRadiusFrame++;
 	}
 
 	private void increaseFade() {
-		double frameProgress = fadeFrames / FADE_FRAMES;
+		double frameProgress = upcomingFadeFrame / (double) (FADE_FRAMES - 1);
 		// Decreasing deceleration
 		double exponential = Math.Pow(1 - frameProgress, FADE_EXPONENT);
 
@@ -36,14 +36,16 @@ class FadeRipple : Ripple {
 
 		alphaMultiplier = exponential;
 
-		fadeFrames++;
+		upcomingFadeFrame++;
 	}
 
-	public override bool onFrameEnd() {
+	public override void onFrameStart() {
 		mayExpandRadius();
 
 		if (!isStillDown) increaseFade();
+	}
 
-		return fadeFrames >= FADE_FRAMES;
+	public override bool onFrameEnd() {
+		return upcomingFadeFrame >= FADE_FRAMES;
 	}
 }
